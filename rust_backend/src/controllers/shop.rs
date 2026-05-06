6db43545440c620f86ba938521006b5544f639c4
Andy Batista Caraballo <andy.batista.caraballo@gmail.com>
2026-05-06 14:23:36 -0400

    Modelo de productos en tienda y endpoint para cargarlos en front

---
+++ rust_backend/src/controllers/shop.rs
@@ -0,0 +1,30 @@
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::unnecessary_struct_initialization)]
+#![allow(clippy::unused_async)]
+use crate::models::_entities::product_template::{self};
+use loco_rs::prelude::*;
+use sea_orm::{query::*, Database};
+
+#[debug_handler]
+pub async fn list(State(_ctx): State<AppContext>) -> Result<Response> {
+    // 1. Conexión a Odoo
+    // Nota: Más adelante moveremos esto a un recurso global para no conectar en cada request
+    let odoo_db = Database::connect("postgres://odoo:postgres@localhost:5432/odoo_prod")
+        .await
+        .map_err(|e| Error::BadRequest(e.to_string()))?;
+
+    // 2. Consulta
+    let products = product_template::Entity::find()
+        .filter(product_template::Column::IsPublished.eq(true))
+        .limit(10)
+        .all(&odoo_db)
+        .await?;
+
+    format::json(products)
+}
+
+pub fn routes() -> Routes {
+    Routes::new()
+        .prefix("api/shop")
+        .add("/", get(list))
+}
---
+++ rust_backend/tests/requests/shop.rs
@@ -0,0 +1,17 @@
+use odoo_shop::app::App;
+use loco_rs::testing::prelude::*;
+use serial_test::serial;
+
+#[tokio::test]
+#[serial]
+async fn can_get_shops() {
+    request::<App, _, _>(|request, _ctx| async move {
+        let res = request.get("/api/shops/").await;
+        assert_eq!(res.status_code(), 200);
+
+        // you can assert content like this:
+        // assert_eq!(res.text(), "content");
+    })
+    .await;
+}
+
