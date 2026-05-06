39405a7c928a930e47a47e9c05e11591991b2068
Andy Batista Caraballo <andy.batista.caraballo@gmail.com>
2026-05-06 14:23:30 -0400

    Modelo de productos en tienda y endpoint para cargarlos en front

--- rust_backend/src/app.rs
+++ rust_backend/src/app.rs
@@ -47,9 +47,11 @@ impl Hooks for App {
 
     fn routes(_ctx: &AppContext) -> AppRoutes {
         AppRoutes::with_default_routes() // controller routes below
+            .add_route(controllers::shop::routes())
             .add_route(controllers::auth::routes())
     }
     async fn connect_workers(ctx: &AppContext, queue: &Queue) -> Result<()> {
+        queue.register(crate::workers::product_sync::Worker::build(ctx)).await?;
         queue.register(DownloadWorker::build(ctx)).await?;
         Ok(())
     }
